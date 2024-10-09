f5d6af2c4370d8a830c851b2132be608fea7b0ec
ngalp <ng.alphania@gmail.com>
2024-10-09 07:55:35 +0000

    test image

--- info.py
+++ info.py
@@ -7,3 +7,21 @@ response = requests.get(url)
 img = Image.open(requests.get(url, stream=True).raw)
 
 st.logo(img,link='https://auditscribe.streamlit.app/', icon_image=img)
+
+import streamlit as st
+import base64
+
+with open(img, "rb") as f:
+    data = base64.b64encode(f.read()).decode("utf-8")
+
+    st.sidebar.markdown(
+        f"""
+        <div style="display:table;margin-top:-20%;margin-left:20%;">
+            <img src="data:image/png;base64,{data}" width="100" height="150">
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.sidebar.header("Part 1")
+    st.sidebar.markdown("Here is some text")
