1e8441c4119e7f45bf05f16130899474ab0c8677
ngalp <ng.alphania@gmail.com>
2024-10-09 03:02:53 +0000

    create new folders

--- info.py
+++ info.py
@@ -1,39 +1,5 @@
 import streamlit as st
 from openai import OpenAI
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
-  
-st.title("📝 Document Q&A with OpenAI")
-uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
-question = st.text_input(
-    "Ask something about the article",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
-
-if uploaded_file and question and not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.")
-
-if uploaded_file and question and openai_api_key:
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Process the uploaded file and question.
-    document = uploaded_file.read().decode()
-    messages = [
-        {
-            "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
-        }
-        ]
-
-    # Generate an answer using the OpenAI API.
-    stream = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        stream=True,
-        )
-
-    # Stream the response to the app using `st.write_stream`.
-    st.write_stream(stream)
+image = Image.open('/path/image.png')
+st.sidebar.image(image, use_column_width=False)
