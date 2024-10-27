8cde738b5137cb11ccf04d64a52860c4ea0f7183
ngalp <ng.alphania@gmail.com>
2024-10-27 12:36:10 +0800

    test logo

--- webapp/logo/icon.py
+++ webapp/logo/icon.py
@@ -2,5 +2,5 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-response = requests.get('https://github.com/ngalp/auditscribe/blob/main/webapp/logo/logo.png?raw=true')
-logo =Image.open(BytesIO(response.content))
+response = requests.get('https://github.com/ngalp/auditscribe/blob/main/webapp/logo/icon.ico?raw=true')
+icon =Image.open(BytesIO(response.content))
--- webapp/logo/logo.py
+++ webapp/logo/logo.py
@@ -1,8 +1,6 @@
-import urllib.request 
-from PIL import Image 
-  
-urllib.request.urlretrieve( 
-  'https://github.com/ngalp/auditscribe/blob/main/webapp/logo/logo.png?raw=true', 
-   "logo.png") 
-  
-logo = Image.open("logo.png") 
+from PIL import Image
+import requests
+from io import BytesIO
+
+response = requests.get('https://github.com/ngalp/auditscribe/blob/main/webapp/logo/logo.ico?raw=true')
+logo =Image.open(BytesIO(response.content))
