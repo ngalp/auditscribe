89166b05d744e49acb6cc7ae1d799cf535a9c9e2
ngalp <ng.alphania@gmail.com>
2024-11-02 01:35:00 +0800

    update completion

--- webapp/classes.py
+++ webapp/classes.py
@@ -1,4 +1,4 @@
-import openai
+from openai import OpenAI
 from langchain_community.llms import HuggingFaceHub
 from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
