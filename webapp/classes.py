0bc9affc5a09b86b780b4fac98e19ef721ca1a12
ngalp <ng.alphania@gmail.com>
2024-11-02 01:32:30 +0800

    update completion

--- webapp/classes.py
+++ webapp/classes.py
@@ -11,13 +11,13 @@ def run_request(question_to_ask, model_type, key, alt_key):
             # Ensure GPT-4 does not include additional comments
             task = task + " The script should only include code, no comments."
         openai.api_key = key
-        response = openai.ChatCompletion.create(model=model_type,
+        response = client.chat.completions.create(model=model_type,
             messages=[{"role":"system","content":task},{"role":"user","content":question_to_ask}])
         llm_response = response["choices"][0]["message"]["content"]
     elif model_type == "text-davinci-003" or model_type == "gpt-3.5-turbo-instruct":
         # Run OpenAI Completion API
         openai.api_key = key
-        response = openai.Completion.create(engine=model_type,prompt=question_to_ask,temperature=0,max_tokens=500,
+        response = client.chat.completions.create(engine=model_type,prompt=question_to_ask,temperature=0,max_tokens=500,
                     top_p=1.0,frequency_penalty=0.0,presence_penalty=0.0,stop=["plt.show()"])
         llm_response = response["choices"][0]["text"] 
     else:
